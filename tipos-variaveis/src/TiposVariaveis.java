e289b49278460dd52c2cf61bf642c238208d32bc
Israel <brenoisrael931@gmail.com>
2025-03-30 10:34:24 -0300

    os tipos e variaveis do java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double SalaioMinimo = 2500;
+        byte idade = 25;
+        short ano = 2024;
+        int populacao = 1500000;
+        long distancia = 9223372036854775807L; // Necessário 'L' no final
+        float preco = 5.99f; // Necessário 'f' no final
+        char letra = 'A';
+        boolean ativo = true;
     }
 }
